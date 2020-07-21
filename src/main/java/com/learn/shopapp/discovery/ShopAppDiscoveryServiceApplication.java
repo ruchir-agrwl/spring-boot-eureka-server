bdecd85f1a6cbc7c68387abd868159e50ad55e51
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-07-21 23:55:49 +0530

    Boiler Plate Code Added Closes #1

---
+++ src/main/java/com/learn/shopapp/discovery/ShopAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learn.shopapp.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAppDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/shopapp/discovery/ShopAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.shopapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
